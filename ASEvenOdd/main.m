159d191127a748abcd13f84b44810efec67e5d77
Afsana Shaikh <afsanashaikh2491@gmail.com>
2016-10-24 15:17:50 +0530

    Modified

--- ASEvenOdd/main.m
+++ ASEvenOdd/main.m
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 void evenodd(int number);
+void evenoddwithoutmod(int number);
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
@@ -15,17 +16,37 @@ int main(int argc, const char * argv[]) {
         printf("\n Enter your number:\n");
         scanf("%d",&number);
         evenodd(number);
+        evenoddwithoutmod(number);
     }
     return 0;
 }
 //function to find even odd
 void evenodd(int number)
 {
+    
     if(number %2 ==0)
     {
         printf("Number %d is even\n",number);
     }
     else{
         printf("Number %d is odd\n",number);
+    }
+}
+//without mod operator
+void evenoddwithoutmod(int number)
+{
+    int divident,result;
+    printf("\nEnter Another number\n");
+    scanf("%d",&number);
+    divident=number/2;
+    result=divident*2;
+    if (result==number) {
+        printf("Number %d is even\n",number);
+
+    }
+    else
+    {
+        printf("Number %d is odd\n",number);
+
     }
 }
